860809e54a661683265ec5cb94597080b4e5dd26
Waleed Ashraf <dedoashraf70@yahoo.com>
2023-05-03 19:19:31 +0200

    app.dart
    just splitted app.dart ( My App)

---
+++ tab_cash_orange/lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      initialRoute: ,
+      onGenerateRoute: ,
+      builder: ,
+      theme: ,
+      title: "Tap Cash",
+
+    );
+  }
+}
