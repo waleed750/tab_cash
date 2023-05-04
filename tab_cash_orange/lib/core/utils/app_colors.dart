5746daf477940d39941221eb3a06f1e16a436873
Waleed Ashraf <dedoashraf70@yahoo.com>
2023-05-04 20:53:20 +0200

    App Colors Added

--- tab_cash_orange/lib/core/utils/app_colors.dart
+++ tab_cash_orange/lib/core/utils/app_colors.dart
@@ -1,3 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tab_cash_orange/core/utils/hex_color.dart';
+
 class AppColors{
+    //App Colors Default
+    static  Color defaultColor =  HexColor('111441');
+    static  Color backgroundColor =  HexColor('FFFFFF');
+    static  Color disabledColor =  HexColor('DDE4EE');
+    static  Color buttonColor =  HexColor('2255EE');
+
+
+    static  Color black =  HexColor('000000');
+    static  Color redOrange =  HexColor('F92729');
+    static  Color wing =  HexColor('FF8321');
+
+
 
 }
