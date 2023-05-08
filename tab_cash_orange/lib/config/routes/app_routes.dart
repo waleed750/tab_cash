9fa16f1607786737ad5a919489b5cfbf0ffa63d9
Waleed Ashraf <dedoashraf70@yahoo.com>
2023-05-09 01:09:03 +0200

    Authentication
    Co-Authored-By: Mahmoud Hanafy <94074275+MahmoudHanaf@users.noreply.github.com>

--- tab_cash_orange/lib/config/routes/app_routes.dart
+++ tab_cash_orange/lib/config/routes/app_routes.dart
@@ -2,19 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../core/utils/app_strings.dart';
+import '../../features/authentication/presentation/screens/splash_screen.dart';
 import '../../features/home/presentation/cubit/home_cubit.dart';
 import '../../features/home/presentation/screens/main_screen.dart';
-import '../../features/login/presentation/screens/splash_screen.dart';
 
 class Routes {
   static const String initialRoute = '/';
+  static const String splashRoute = '/splash';
   static const String homeRoute = '/home';
 }
 
 class AppRoutes {
   static Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case Routes.initialRoute:
+      case Routes.splashRoute:
         return MaterialPageRoute(
           builder: (context) => SplashScreen(),
         );
